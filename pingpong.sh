9d95ff1bb5594fe43b2b5d158a8ff1922e308252
Muhammad Aprian <sec@nairpaa.me>
2020-07-16 22:03:03 +0700

    Create pingpong.sh and add script to multiple ping

---
+++ pingpong.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+ip_list=$1
+
+# check file
+if [ -e $ip_list ] && [ ! -z $1 ]
+then
+    
+    # check empty file
+    if [ -s $ip_list ]
+    then
+        
+        # looping for get ip target
+        for ip in `cat $ip_list`
+        do
+            # ping to target
+            ping -c1 $ip &> /dev/null
+            
+            # check connection live or dead
+            if [ $? -eq 0 ]
+            then
+                echo "$ip is Live"
+            else
+                echo "$ip is Dead"
+            fi
+        done
+        
+    else
+        echo "File $ip_list is empty."
+    fi
+    
+else
+    
+    # check whether using arguments?
+    if [ -z $1 ]
+    then
+        echo "argument not found\n"
+        
+        echo "Usage:"
+        echo "./pingpong.sh ip-list.txt"
+    else
+        echo "File $ip_list not found."
+    fi
+    
+fi
